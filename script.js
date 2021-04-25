4212349058d341cb9ff5ccbfb29a529875aed6b2
Willi Dehlinger <noopinator89@gmail.com>
2021-04-25 12:32:47 +0200

    Changed README + Solution 1.1

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ $(document).ready(() => {
   // If we dont "wait" for document to be ready, we cannot access HTML elements
   // for testing purposes, you can use a "debugger;" statement or also "console.log(element)"
   console.log("DOM is ready!");
+
+  const form = $('#hft-shoutbox-form');
+  const alertBox = $('#hft-shoutbox-alert');
+
+  form.on('keyup', (event) => {
+    const formData = new FormData(event.currentTarget);
+    const usernameLength = formData.get("username").length;
+    const messageLength = formData.get("message").length;
+    const submitButton = document.getElementById('hft-shoutbox-form-submit');
+
+    console.log(usernameLength, messageLength, usernameLength < 3, messageLength < 10);
+    if (usernameLength < 3 || messageLength < 10) {
+      alertBox.removeClass("d-none");
+      alertBox.addClass("d-block");
+      submitButton.setAttribute('disabled','disabled');
+    } else {
+      alertBox.addClass("d-none");
+      alertBox.removeClass("d-block");
+      submitButton.removeAttribute('disabled');
+    }
+  })
+
 });
